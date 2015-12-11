2fc53bf12ea5248b7fd1df1ef68998d23b7a7c93
glisterkaiser <glisterkaiser@gmail.com>
2015-12-11 10:28:57 -0430

    adding first scripts

---
+++ Assets/Scripts/generator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
