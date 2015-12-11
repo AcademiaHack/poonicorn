979ec1cbd71c9ea270802529f9d1db1c3d3e0452
glisterkaiser <glisterkaiser@gmail.com>
2015-12-11 15:03:25 -0430

    gamestate added

---
+++ Assets/Scripts/gamestateScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class gamestateScript : MonoBehaviour {
+
+    float counter = 0.0f;
+    public Text pointsLabel;
+	
+	// Update is called once per frame
+	void Update () {
+        counter += 1 * Time.deltaTime;
+
+        pointsLabel.text = "Time: " + (Mathf.Round(counter * 10.0f) / 10.0f)+"s";
+	}
+
+    
+}
