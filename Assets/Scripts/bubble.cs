84a412213523a9d85eacf058a39d34f8ad931915
glisterkaiser <glisterkaiser@gmail.com>
2015-12-11 13:11:54 -0430

    prefabbing camera, with buble generator on it, adding script for the generator and the autodestroy bubbles

---
+++ Assets/Scripts/bubble.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bubble : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(this.transform.position.x +speed, this.transform.position.y, this.transform.position.z);
+
+        if (this.transform.position.x < -10) {
+            Destroy(this.gameObject);
+        }
+	}
+}
--- Assets/Scripts/generator.cs
+++ Assets/Scripts/generator.cs
@@ -2,14 +2,25 @@
 using System.Collections;
 
 public class generator : MonoBehaviour {
+    System.Random r = new System.Random();
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GameObject enemy;
+    public float interval; 
+    
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("GenerateBestSmellEver", 0.2f, interval);
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    void GenerateBestSmellEver() {
+        Instantiate(enemy, new Vector3(this.transform.position.x, this.transform.position.y + (float)rnd(-5.0f, 5.0f), 0), Quaternion.identity);
+    }
+
+    double rnd(double a, double b) {
+        return a + r.NextDouble() * (b - a);
+    }
 }
