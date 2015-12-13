832bc2fb427beb2cdab295b7248f72b32c9f5e5d
jgab <jgab.net@gmail.com>
2015-12-12 21:43:17 -0430

    tratando de acomodar el peo de los nombre de clase

--- Assets/GameStateScript.cs
+++ Assets/GameStateScript.cs
@@ -7,38 +7,38 @@ public class GameStateScript : MonoBehaviour {
 	
 	public Text pointsLabel;
 	public Text pooLabel;
-
+	
 	public int freePooLimit = 8;
 	public int defecateLimit = 3;
-
+	
 	public float bubbleSpeed = -0.2f; //-0.15f;
 	public float bubbleSpawnerInterval = 1f;
-
+	
 	float counter = 0.0f;
 	float time;
-
+	
 	float bubbleSpeedChange = 8f;
 	int lastBubbleSpeedChange = 0;
 	
 	Generator generator;
-
+	
 	void Start () {
 		generator = GameObject.Find ("EnemySpawner").GetComponent<Generator> ();	
 		generator.ProgrameSpawner(bubbleSpawnerInterval);
 	}
-
+	
 	void Update () {
-        counter += 1 * Time.deltaTime;
+		counter += 1 * Time.deltaTime;
 		time = Mathf.Round (counter * 10.0f) / 10.0f;
-
-        pointsLabel.text = "Time: " + time +"s";
-
-        Debug.Log(((int)time) + " -- " + bubbleSpeedChange);
-    	if ( lastBubbleSpeedChange != (int)time && ( 
-			((int)time > bubbleSpeedChange && (int)time % bubbleSpeedChange == 0) || 
-			(int)time == bubbleSpeedChange)
-		) {
-    		lastBubbleSpeedChange = (int)time;
+		
+		pointsLabel.text = "Time: " + time +"s";
+		
+		Debug.Log(((int)time) + " -- " + bubbleSpeedChange);
+		if ( lastBubbleSpeedChange != (int)time && ( 
+		                                            ((int)time > bubbleSpeedChange && (int)time % bubbleSpeedChange == 0) || 
+		                                            (int)time == bubbleSpeedChange)
+		    ) {
+			lastBubbleSpeedChange = (int)time;
 			Debug.Log("Multiplo");
 			bubbleSpeed -= 0.004f;		
 			if (bubbleSpawnerInterval > 0.01f) {
@@ -50,20 +50,20 @@ public class GameStateScript : MonoBehaviour {
 			generator.ProgrameSpawner(bubbleSpawnerInterval);
 		}		
 	}
-
+	
 	public void changePoo(int poo) {
 		pooLabel.text = "Poo: " + poo;
-
+		
 		if (poo >= freePooLimit) {
 			pooLabel.color = Color.red;
 		} else {
 			pooLabel.color = Color.gray;
 		}
 	}    
-
+	
 	public void saveme () {
 		bubbleSpeed += 0.01f;
 		changePoo (0);
 	}
-
+	
 }
---
+++ Assets/Generator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour {
+	System.Random r = new System.Random();
+	
+	public GameObject enemy;
+	
+	GameStateScript state;
+	
+	public void ProgrameSpawner(float interval) {
+		CancelInvoke("GenerateBestSmellEver");
+		InvokeRepeating("GenerateBestSmellEver", 0.2f, interval);		
+	}
+	
+	public void GenerateBestSmellEver() {
+		Instantiate(enemy, new Vector3(this.transform.position.x, this.transform.position.y + (float)rnd(-5.0f, 5.0f), 0), Quaternion.identity);
+	}
+	
+	double rnd(double a, double b) {
+		return a + r.NextDouble() * (b - a);
+	}
+}
